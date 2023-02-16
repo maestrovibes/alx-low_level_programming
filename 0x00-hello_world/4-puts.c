fbaff9c2d5b1848da485ebf3069d6a93ae5b4554
maestrovibes <122865994+maestrovibes@users.noreply.github.com>
2023-02-16 13:45:20 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * uses function puts
- * return value is 0
+ * main - entry point
+ * Return:  value is 0
  */
 
 int main(void)
 {
 
 	puts("\"Programming is like building a multilingual puzzle");
-		return (0);
+		return (0);/*returns 0*/
 }
